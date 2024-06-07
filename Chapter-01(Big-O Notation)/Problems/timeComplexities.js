8982f485e7150a60f6dfebb913751d64773ecfce
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-06-08 00:55:13 +0530

    Making changes in the timeComplexities.js file which is in chanpter -1 in problems repo

--- Chapter-01(Big-O Notation)/Problems/timeComplexities.js
+++ Chapter-01(Big-O Notation)/Problems/timeComplexities.js
@@ -21,11 +21,11 @@ for (let i = 0; i < 3; i++) {
 // O(n^3) :- Cubic time     (less data inputs)
 
 for (let i = 0; i < 4; i++) {
-    console.log("i is : ", i);
+    // console.log("i is : ", i);
     for (let j = i; j < 4; j++) {
-        console.log("j is : ", j);
+        // console.log("j is : ", j);
         for (let k = j; k < 4; k++) {
-            console.log("k is : ", k);
+            // console.log("k is : ", k);
         }
     }
 }
@@ -34,3 +34,18 @@ for (let i = 0; i < 4; i++) {
 /**
  * Nested loop cause high time complexity. 
  */
+
+
+// O(log n)  :- Logrithmic time complexity 
+
+function checkLogComplexity(n) {
+    for (let i = 2; i < n; i = (i * 2)) {
+        console.log("i is : ", i)
+    }
+}
+
+// checkLogComplexity(10)
+
+//Note :- The efficiency of logarithmic time complexities is apparent with large inputs such as a million items. 
+
+checkLogComplexity(100000000)
