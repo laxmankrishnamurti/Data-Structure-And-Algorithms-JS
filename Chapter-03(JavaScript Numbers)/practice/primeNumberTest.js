c5d86a13feafd68ba74ba77115fa7afa41bfd059
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-07-01 13:20:59 +0530

    Adding new files

--- Chapter-03(JavaScript Numbers)/practice/primeNumberTest.js
+++ Chapter-03(JavaScript Numbers)/practice/primeNumberTest.js
@@ -13,4 +13,32 @@ function primeNumberTest(n) {
     return true
 }
 
-console.log(primeNumberTest(20))
+console.log("From primeNumberTest function :: ", primeNumberTest(2321555))
+
+function checkPrime(n) {
+
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+
+    if ((n % 2 === 0) || (n % 3 === 0)) return false
+
+    let sqr = Math.floor(Math.sqrt(n))
+    const checkArr = [];
+
+    while (sqr >= 5) {
+        if ((sqr % 3 !== 0)) {
+            checkArr.push(sqr)
+        }
+        sqr -= 2;
+    }
+
+    for (let i = 0; i < checkArr.length; i++) {
+        if ((n % checkArr[i] === 0)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log("From checkPrime function :: ", checkPrime(2321555))
