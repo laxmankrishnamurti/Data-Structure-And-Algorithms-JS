843da682a346ae9961b4c28acca491d19885a689
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-07-09 21:25:55 +0530

    Making changes in the file

--- Chapter-04(JavaScript Strings)/practice/basicOfString.js
+++ Chapter-04(JavaScript Strings)/practice/basicOfString.js
@@ -3,3 +3,6 @@ console.log(username.charAt(1))     //output :- a
 
 console.log(username.substring(0,6))    //output :- Laxman
 console.log(username.substring(0))      //output :- Laxman Krishnamurt
+
+//Note :- If we don't pass the final index value(last index) in the substring() function it will return all the character 
+//values from the specified start position untill the end.
