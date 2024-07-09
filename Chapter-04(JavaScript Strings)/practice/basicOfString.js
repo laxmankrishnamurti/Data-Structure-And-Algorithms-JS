5fe4d6fcc0559c6f0210b0f40cccaf9eeea7229a
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-07-09 21:23:51 +0530

    Making changes in the file

--- Chapter-04(JavaScript Strings)/practice/basicOfString.js
+++ Chapter-04(JavaScript Strings)/practice/basicOfString.js
@@ -2,3 +2,4 @@ let username = "Laxman Krishnamurti"
 console.log(username.charAt(1))     //output :- a
 
 console.log(username.substring(0,6))    //output :- Laxman
+console.log(username.substring(0))      //output :- Laxman Krishnamurt
