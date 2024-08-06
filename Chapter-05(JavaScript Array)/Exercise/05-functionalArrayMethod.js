2c44b2e06d2e8e0dc795a73df830c361d5faf534
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-08-06 11:05:44 +0530

    Adding important functional method of an array

--- Chapter-05(JavaScript Array)/Exercise/05-functionalArrayMethod.js
+++ Chapter-05(JavaScript Array)/Exercise/05-functionalArrayMethod.js
@@ -2,8 +2,33 @@
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+//map() Method
+
 const getMultiplyBy5Value = numbers.map((number) => {
   return number * 5;
 });
 
 console.log("Array with multiply by 5 : ", getMultiplyBy5Value);
+
+//filter() method
+
+const getEvenNumbers = numbers.filter((number) => number % 2 === 0);
+console.log("Array of even numbers :: ", getEvenNumbers);
+
+//reduce() method
+
+//prev will become undefined if we do not return anything.
+
+numbers.reduce((prev, next, index, arr) => {
+  // console.log("prev :: ", prev);
+  // console.log("next:: ", next);
+  // console.log("index :: ", index);
+  // console.log("arr :: ", arr);
+  return next;
+});
+
+const getSum = numbers.reduce((prev, next) => {
+  return prev + next;
+}, 0);
+
+console.log("sum of the array is : ", getSum);
