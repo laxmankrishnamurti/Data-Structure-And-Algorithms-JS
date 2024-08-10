4052ae5a83f0e0bab0e9b204c4ae5854b725fd99
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-08-10 16:57:32 +0530

    Adding fibonacci solution with recursion

--- Chapter-08(Recursion)/Practice/01-BaseCaseOfRecursion.js
+++ Chapter-08(Recursion)/Practice/01-BaseCaseOfRecursion.js
@@ -24,3 +24,55 @@ let factorialValue = getFactorialOfN(5);
 console.log("Factorial of 5 is : ", factorialValue);
 
 //Factorial of 5 is :  120
+
+//Let's calculate Fibonacci Sequence
+
+/**
+ * Solution - 01 (With Iteration)
+ */
+
+function getNthFibonacciNumber(n) {
+  //edge case
+  if (n <= 1) {
+    return n;
+  }
+
+  let nthFibonacciNumber = 0,
+    last = 1,
+    lastlast = 0;
+
+  for (let i = 2; i < n; i++) {
+    nthFibonacciNumber = last + lastlast;
+    lastlast = last;
+    last = nthFibonacciNumber;
+  }
+
+  return nthFibonacciNumber;
+}
+
+let position = 8;
+let fibonacciNumber = getNthFibonacciNumber(position);
+console.log(`${position}th fibonacci number is : ${fibonacciNumber}`);
+
+/**
+ * Solution - 02 (With Recursion)
+ */
+
+function getNthFibonacciNumberWithRecursion(n) {
+  //Base case
+  if (n <= 1) {
+    return n;
+  }
+
+  return (
+    getNthFibonacciNumberWithRecursion(n - 1) +
+    getNthFibonacciNumberWithRecursion(n - 2)
+  );
+}
+
+let position2 = 3;
+let fibonacciNumberWithRecursion =
+  getNthFibonacciNumberWithRecursion(position2);
+console.log(
+  `${position2}th fibonacci number is : ${fibonacciNumberWithRecursion}`
+);
