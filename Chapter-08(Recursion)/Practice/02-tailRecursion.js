09d4d56b8f3b8305c7d0a0178320af553ce7e23c
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-08-11 21:55:21 +0530

    Adding tail recursion method

--- Chapter-08(Recursion)/Practice/02-tailRecursion.js
+++ Chapter-08(Recursion)/Practice/02-tailRecursion.js
@@ -5,7 +5,7 @@ function getNthFibonacciNumber(n) {
     last = 1,
     lastlast = 0;
 
-  for (let i = 2; i <= n; i++) {
+  for (let i = 2; i < n; i++) {
     fibNumber = lastlast + last;
     lastlast = last;
     last = fibNumber;
@@ -14,4 +14,19 @@ function getNthFibonacciNumber(n) {
   return fibNumber;
 }
 
-console.log("2nd fibonacci number is : ", getNthFibonacciNumber(2));
+console.log("51 fibonacci number is : ", getNthFibonacciNumber(51));
+
+function getNthFibonacciNumberTailRecursion(n, lastlast, last) {
+  if (n == 2) {
+    return last;
+  }
+
+  return getNthFibonacciNumberTailRecursion(n - 1, last, lastlast + last);
+}
+
+console.log(getNthFibonacciNumberTailRecursion(51, 0, 1));
+
+// Tail Recursive function :: As recursive function get closer to the base case the value will be also get evaluate and once the base case is reached the final result will be returned.
+
+//Time Complexity -> O(n)
+//Space Complexity -> O(n)
