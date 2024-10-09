ab0647d25c67f9163446b568d6192ea7e11b84b4
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-10-09 14:32:23 +0530

    Starting solving pattern problems

---
+++ DSA Modules/01-Learning the basics/module-02.js
@@ -0,0 +1,50 @@
+function pattern1(n){
+    let str = "*";
+    let i = 1;
+    while(i <= n){
+        console.log(str.repeat(n));
+        i++;
+    }
+}
+// pattern1(5)
+
+function pattern2(n){
+    let str = "*";
+    let i = 1; 
+    while(i <= n){
+        console.log(str.repeat(i));
+        i++;
+    }
+}
+// pattern2(5)
+
+function pattern3(n){
+    let str = ""
+    let i = 1; 
+    while(i <= n){
+        str += `${i} `;
+        console.log(str)
+        i++;
+    }
+}
+// pattern3(5)
+
+function pattern4(n){
+    let i = 1;
+    let str = "";
+    while(i <= n){
+        str = `${i} `
+        console.log(str.repeat(i))
+        i++;
+    }
+}
+// pattern4(5)
+
+function pattern5(n){
+    let str = "* ";
+    while(n){
+        console.log(str.repeat(n));
+        n--;
+    }
+}
+pattern5(5)
