346a53d1628fd9243edde56c1fe8372a6e3f3a7a
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-10-12 04:04:36 +0530

    SOLVING DSA PROBLEMS

---
+++ DSA Modules/01-Learning the basics/Module-04.md
@@ -0,0 +1,1 @@
+_1. Count Digits problem_
---
+++ DSA Modules/01-Learning the basics/module-04.js
@@ -0,0 +1,70 @@
+function evenlyDivides(N){
+    let count = 0; 
+    let num = N;
+    let digit;
+    while(num){
+        digit = Math.floor(num % 10);
+        num = Math.floor(num / 10);
+        if(digit !== 0){
+            let isDivisible = N / digit
+            if(N % digit === 0){
+                count++
+            }
+        }
+    }
+    return count;
+}
+// let digits = evenlyDivides(2446)
+// console.log(digits)
+
+function reverseInteger(x){
+    let isNegative = false;
+    if(x < 0){
+        isNegative = true;
+        x = x - (x * 2)
+    }
+
+    let digit;
+    let reverse = 0;
+    let num = x;
+    while(num){
+        digit = Math.floor(num % 10);
+        num = Math.floor(num / 10);
+        reverse = (reverse * 10) + digit;
+    }
+    if(isNegative){
+        return `-${reverse}`
+    }
+    return reverse;
+}
+// console.log(reverseInteger(-123))
+
+function lcmAndGcd(a, b){
+    let gcd;
+    let lcm;
+    let dividend;
+    let divisor;
+    let remainder;
+    if(a > b){
+        dividend = a;
+        divisor = b;
+    }else {
+        dividend = b;
+        divisor = a;
+    }
+    while(divisor !== 1 || remainder !== 0){
+        remainder = dividend % divisor;
+        if(remainder !== 0){
+            dividend = divisor;
+            divisor = remainder;
+        }else {
+            break;
+        }
+    }
+    
+    gcd = divisor;
+    lcm = (a * b) / gcd;
+    return `${lcm} ${gcd}`
+}
+
+console.log(lcmAndGcd(354293,125208))
