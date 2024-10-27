5d45d4cc5e6748ded9f9e5649f01ddf087f5d7a9
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-10-27 13:54:20 +0530

    practice on recursion

--- 02-Practice/01-Learning the basics/05(Recursion)/recursion.js
+++ 02-Practice/01-Learning the basics/05(Recursion)/recursion.js
@@ -29,4 +29,50 @@ function printStr(n, origin = n) {
   }
 }
 
-printStr(10);
+// printStr(10);
+
+// 4. Print numbers from N to 1 (space separated) without the help of loops.
+
+function printDec(n, origin = n) {
+  if (n === 0) {
+    return;
+  }
+  process.stdout.write(n + " ");
+  printDec(n - 1, origin);
+
+  if (n === origin) {
+    process.stdout.write("\n");
+  }
+}
+
+// printDec(10);
+
+// 5. Given an integer n, calculate the sum of series 1^3 + 2^3 + 3^3 + 4^3 + … till n-th term.
+
+function sumOfSeries(n) {
+  if (n === 1) {
+    return 1;
+  }
+
+  return n ** 3 + sumOfSeries(n - 1);
+}
+
+console.log(sumOfSeries(3));
+
+//Without recursion
+
+function sumOfCubes(n) {
+  // Use BigInt to handle large numbers accurately
+  const bigN = BigInt(n);
+  const sumOfFirstN = (bigN * (bigN + 1n)) / 2n; // `1n` is BigInt syntax for 1
+  return sumOfFirstN ** 2n;
+}
+
+// Example usage
+// const n = 26501;
+// console.log("Sum of series:", sumOfCubes(n).toString()); // Convert BigInt result to string for display
+// For BigInt syntax, integers are suffixed with n(e.g., 1n), and all operations are performed using BigInt methods.
+
+/**
+ * Conclusion ===> If upper frame has to return something to the below frame the recursive expression should be expressed with the return keyword.
+ */
