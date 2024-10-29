019c9d12b81125bced6f37fd2dcc9841ab867904
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-10-29 14:03:03 +0530

    modifying getHashTable function logic by adding a pre-built function to find the max value in the input array which decides the length of the hashtable

--- 02-Practice/01-Learning the basics/06(Hashing)/hashing.js
+++ 02-Practice/01-Learning the basics/06(Hashing)/hashing.js
@@ -1,8 +1,7 @@
-function hashTable(arr) {
-  let maxHashNumber = 6;
-  let hashTable = Array(maxHashNumber).fill(0);
+function getHashTable(arr) {
+  let maxHashNumber = Math.max(...arr);
+  let hashTable = Array(maxHashNumber + 1).fill(0);
 
-  console.log(hashTable);
   for (let i = 0; i < arr.length; i++) {
     hashTable[arr[i]] += 1;
   }
@@ -14,5 +13,5 @@ let array = [
   1, 2, 3, 4, 5, 4, 3, 2, 3, 4, 5, 4, 3, 2, 3, 4, 3, 2, 1, 2, 3, 2, 4, 5, 4, 3,
   2, 3,
 ];
-let hashT = hashTable(array);
+let hashT = getHashTable(array);
 console.log("Occurance of 5", hashT[5]);
