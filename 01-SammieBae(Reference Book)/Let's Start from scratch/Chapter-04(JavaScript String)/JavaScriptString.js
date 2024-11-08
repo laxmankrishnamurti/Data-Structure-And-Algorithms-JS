60c9b7be266ae1e6629165912abea822d7dadefc
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-11-08 17:32:47 +0530

    started javascript stirng

--- 01-SammieBae(Reference Book)/Let's Start from scratch/Chapter-04(JavaScript String)/JavaScriptString.js
+++ 01-SammieBae(Reference Book)/Let's Start from scratch/Chapter-04(JavaScript String)/JavaScriptString.js
@@ -6,3 +6,28 @@ console.log("name is", name1);
 
 const name2 = "Laxmna";
 console.log("name is", name2);
+
+/**
+ * String access
+ */
+
+// In every programming language indexing starts from 0
+
+// Single character
+
+const email = "laxmankrishnamurti@gmail.com";
+const index = 2;
+console.log(`Character at ${index} index in ${email} is`, email.charAt(index));
+
+// Output -> Character at 2 index in laxmankrishnamurti@gmail.com is x
+
+// Get a sub-string or a group of character
+
+const start = 3;
+const end = 10; // (excluded)
+console.log(
+  `Sub-String from ${start} to ${end} in ${email} is`,
+  email.substring(start, end)
+);
+
+// Output -> Sub-String from 3 to 10 in laxmankrishnamurti@gmail.com is mankris
