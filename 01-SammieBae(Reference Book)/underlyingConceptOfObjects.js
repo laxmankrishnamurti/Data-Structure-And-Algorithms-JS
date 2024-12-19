c7a66c64abbcf83493ac92fc76c23d99555fd37a
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-12-19 15:32:54 +0530

    DSA: JAVASCRIPT OBJECT

--- 01-SammieBae(Reference Book)/underlyingConceptOfObjects.js
+++ 01-SammieBae(Reference Book)/underlyingConceptOfObjects.js
@@ -21,3 +21,11 @@ user {
   greet: [Function: greet]
 }
 */
+
+// In JavaScript, every object has an internal link to another object called its prototype, which forms a prototype chain.
+
+const firstPrototype = Object.getPrototypeOf(user);
+console.log("firstPrototype of user", firstPrototype);
+
+// firstPrototype of user [Object: null prototype] {}
+// why :: The REPL (Read-Eval-Print Loop) gives more explicit debugging information, including prototype metadata.
